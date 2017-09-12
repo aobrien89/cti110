3f383877328f8ec34c13ba17d1fe4b96a3142b8b
aobrien89 <31904114+aobrien89@users.noreply.github.com>
2017-09-12 17:56:11 -0400

    Add files via upload
    First Submission

---
+++ M2T1_SalesPrediction_O'Brien.py
@@ -0,0 +1,9 @@
+# CTI-110 
+# M2T1 - Sales Prediction 
+# Anthony O'Brien
+# 09/05/2017
+#
+
+print("Input Total Sales")
+totalsales = float(input("Total Sales: "))
+print("Annual Profit", (totalsales*.23))
